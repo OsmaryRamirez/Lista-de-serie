583da6fa22a6dc578b73e0bd55bc4158e7858fd8
Osmary <Osmary@DESKTOP-8DOM8BM>
2024-02-08 21:22:43 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace app_listbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lst_series = new System.Windows.Forms.ListBox();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.txt_series = new System.Windows.Forms.TextBox();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.lbl_indice = new System.Windows.Forms.Label();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "series";
+            // 
+            // lst_series
+            // 
+            this.lst_series.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lst_series.FormattingEnabled = true;
+            this.lst_series.ItemHeight = 25;
+            this.lst_series.Location = new System.Drawing.Point(236, 117);
+            this.lst_series.Name = "lst_series";
+            this.lst_series.Size = new System.Drawing.Size(223, 179);
+            this.lst_series.TabIndex = 1;
+            this.lst_series.SelectedIndexChanged += new System.EventHandler(this.lst_series_SelectedIndexChanged);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(465, 168);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(133, 39);
+            this.btn_salir.TabIndex = 2;
+            this.btn_salir.Text = "salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_agregar.Location = new System.Drawing.Point(465, 223);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(133, 34);
+            this.btn_agregar.TabIndex = 3;
+            this.btn_agregar.Text = "agregar";
+            this.btn_agregar.UseVisualStyleBackColor = true;
+            this.btn_agregar.Click += new System.EventHandler(this.btn_agregar_Click);
+            // 
+            // txt_series
+            // 
+            this.txt_series.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_series.Location = new System.Drawing.Point(471, 117);
+            this.txt_series.Name = "txt_series";
+            this.txt_series.Size = new System.Drawing.Size(166, 30);
+            this.txt_series.TabIndex = 4;
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(465, 263);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(133, 34);
+            this.btn_limpiar.TabIndex = 6;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // lbl_indice
+            // 
+            this.lbl_indice.AutoSize = true;
+            this.lbl_indice.Location = new System.Drawing.Point(137, 190);
+            this.lbl_indice.Name = "lbl_indice";
+            this.lbl_indice.Size = new System.Drawing.Size(44, 16);
+            this.lbl_indice.TabIndex = 7;
+            this.lbl_indice.Text = "label2";
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_eliminar.Location = new System.Drawing.Point(465, 303);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(133, 34);
+            this.btn_eliminar.TabIndex = 8;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(909, 510);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.lbl_indice);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.txt_series);
+            this.Controls.Add(this.btn_agregar);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.lst_series);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lst_series;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.TextBox txt_series;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Label lbl_indice;
+        private System.Windows.Forms.Button btn_eliminar;
+    }
+}
+
